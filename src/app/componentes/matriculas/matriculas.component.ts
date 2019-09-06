02e62e4db2eeaa976bd9ede22fb36d277e60cf47
FakeWing <carlos.moralesu@usach.cl>
2019-09-06 11:17:50 -0400

    dropdown menu
    alert selected id

--- src/app/componentes/matriculas/matriculas.component.ts
+++ src/app/componentes/matriculas/matriculas.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiserviciosService } from 'src/app/servicios/apiservicios.service';
+import { TalleresInterface, TalleresClase } from 'src/app/modelos/talleres';
+import { MatriculasInterface , MatriculasClase } from 'src/app/modelos/matriculas';
+import { TalleresComponent } from 'src/app/componentes/talleres/talleres.component';
 
 @Component({
   selector: 'app-matriculas',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatriculasComponent implements OnInit {
 
-  constructor() { }
+selected=null;
+
+public talleres : TalleresInterface;
+
+
+  constructor(private apiServicio:ApiserviciosService) { }
 
   ngOnInit() {
+    
+    this.getTalleres();
+
   }
+
+  public getTalleres() {
+    this.apiServicio.getTalleres().subscribe((talleres: TalleresInterface) => (this.talleres = talleres));
+
+  }
+
+
+  onOptionsSelected(event){
+    let value = event.target.value;
+     this.selected = value;
+     alert(this.selected);
+     
+ }
 
 }
